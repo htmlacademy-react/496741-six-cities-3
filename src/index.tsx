c42635a8e0350b20d9b173390028ebafc2c0ae23
Vagiza Svidchenkova <w.svidchenkova@gmail.com>
2025-05-29 16:32:08 +0400

    Добавляет моковые reviews

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './view/components/app/app';
-import { mockOffers } from './model/mock.ts';
-import { OfferType } from './types/types.ts';
+import { mockReviews, mockOffers } from './model/mock.ts';
+import { ReviewType, OfferType } from './types/types.ts';
 import { CityName } from './const.ts';
 
 const DEFAULT_ACTIVE_LOCATION: CityName = CityName.Amsterdam;
 
 const offers: OfferType[] = mockOffers;
+const reviews: ReviewType[] = mockReviews;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -16,7 +17,11 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <React.StrictMode>
-      <App offers = {offers} activeLocation={DEFAULT_ACTIVE_LOCATION}/>
+      <App
+        offers={offers}
+        reviews={reviews}
+        activeLocation={DEFAULT_ACTIVE_LOCATION}
+      />
     </React.StrictMode>
   </React.StrictMode>
 );
--- src/view/pages/offer/offer.tsx
+++ src/view/pages/offer/offer.tsx
@@ -1,10 +1,12 @@
-import { OfferType } from '../../../types/types.tsx';
+import { ReviewType, OfferType } from '../../../types/types.tsx';
+import ReviewsItem from '../../components/reviews-item/reviews-item.tsx';
 
 type OfferProps = {
   offer: OfferType;
+  reviews: ReviewType[];
 }
 
-function Offer({offer}: OfferProps): JSX.Element {
+function Offer({offer, reviews}: OfferProps): JSX.Element {
   const {
     // id,
     // city,
@@ -147,28 +149,9 @@ function Offer({offer}: OfferProps): JSX.Element {
             <section className="offer__reviews reviews">
               <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
               <ul className="reviews__list">
-                <li className="reviews__item">
-                  <div className="reviews__user user">
-                    <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                      <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar" />
-                    </div>
-                    <span className="reviews__user-name">
-                      Max
-                    </span>
-                  </div>
-                  <div className="reviews__info">
-                    <div className="reviews__rating rating">
-                      <div className="reviews__stars rating__stars">
-                        <span style={{width: '80%'}}></span>
-                        <span className="visually-hidden">Rating</span>
-                      </div>
-                    </div>
-                    <p className="reviews__text">
-                      A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
-                    </p>
-                    <time className="reviews__time" dateTime="2019-04-24">April 2019</time>
-                  </div>
-                </li>
+                {reviews.map((review) => (
+                  <ReviewsItem key={review.id} review={review} />
+                ))}
               </ul>
               <form className="reviews__form form" action="#" method="post">
                 <label className="reviews__label form__label" htmlFor="review">Your review</label>
