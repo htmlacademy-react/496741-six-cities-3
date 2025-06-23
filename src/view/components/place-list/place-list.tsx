98c4ff8df8ba502ff0cc4e4594ab27fbfb4a58e0
Vagiza Svidchenkova <w.svidchenkova@gmail.com>
2025-06-24 00:35:49 +0400

    bug: сбрасывает скролл

--- src/view/components/place-list/place-list.tsx
+++ src/view/components/place-list/place-list.tsx
@@ -1,28 +1,58 @@
-import { OfferType } from '../../../types/types.ts';
+import { useEffect, useRef } from 'react';
+import { CityType, OfferType } from '../../../types/types.ts';
 import PlaceCard from '../place-card/place-card.tsx';
 
 type PlaceListProps = {
   offers: OfferType[];
   onOfferHover: (offer?: OfferType) => void;
+  activeLocation: CityType;
 }
 
-function PlaceList({offers, onOfferHover}: PlaceListProps): JSX.Element {
+function PlaceList({offers, onOfferHover, activeLocation}: PlaceListProps): JSX.Element {
 
   const handleOfferHover = (offer?: OfferType) => {
     onOfferHover(offer || undefined);
   };
 
+  const scrollRef = useRef<HTMLElement>(null);
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = 0;
+    }
+  }, [activeLocation]);
+
   return (
-    <div className="cities__places-list places__list tabs__content">
-      {offers.map((offer) => (
-        <PlaceCard
-          key={offer.id}
-          offer={offer}
-          onOfferHover={handleOfferHover}
-          cardName='CITIES'
-        />
-      ))}
-    </div>
+    <section className="cities__places places" ref={scrollRef}>
+      <h2 className="visually-hidden">Places</h2>
+      <b className="places__found">
+        {`${offers.length} places to stay in ${activeLocation.name}`}
+      </b>
+      <form className="places__sorting" action="#" method="get">
+        <span className="places__sorting-caption">Sort by</span>
+        <span className="places__sorting-type" tabIndex={0}>
+          Popular
+          <svg className="places__sorting-arrow" width="7" height="4">
+            <use xlinkHref="#icon-arrow-select"></use>
+          </svg>
+        </span>
+        <ul className="places__options places__options--custom places__options--opened">
+          <li className="places__option places__option--active" tabIndex={0}>Popular</li>
+          <li className="places__option" tabIndex={0}>Price: low to high</li>
+          <li className="places__option" tabIndex={0}>Price: high to low</li>
+          <li className="places__option" tabIndex={0}>Top rated first</li>
+        </ul>
+      </form>
+      <div className="cities__places-list places__list tabs__content">
+        {offers.map((offer) => (
+          <PlaceCard
+            key={offer.id}
+            offer={offer}
+            onOfferHover={handleOfferHover}
+            cardName='CITIES'
+          />
+        ))}
+      </div>
+    </section>
   );
 }
 
