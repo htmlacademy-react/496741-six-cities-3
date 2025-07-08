2621e5fda612f8165cd2439eea3c31720a4e8d3d
Vagiza Svidchenkova <w.svidchenkova@gmail.com>
2025-07-08 12:06:17 +0400

    Добавит компонет-обертку HistoryRouter над Router

---
+++ src/view/components/history-router/history-router.tsx
@@ -0,0 +1,34 @@
+import type { BrowserHistory } from 'history';
+import { ReactNode, useLayoutEffect, useState } from 'react';
+import { Router } from 'react-router-dom';
+
+export interface HistoryRouterProps {
+  history: BrowserHistory;
+  basename?: string;
+  children?: ReactNode;
+}
+
+function HistoryRouter ({
+  history,
+  basename,
+  children}: HistoryRouterProps) {
+  const [state, setState] = useState({
+    action: history.action,
+    location: history.location,
+  });
+
+  useLayoutEffect(() => history.listen(setState), [history]);
+
+  return (
+    <Router
+      basename={basename}
+      location={state.location}
+      navigationType={state.action}
+      navigator={history}
+    >
+      {children}
+    </Router>
+  );
+}
+
+export default HistoryRouter;
