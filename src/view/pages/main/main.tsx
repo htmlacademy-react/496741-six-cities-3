a9f0eb4cc7f087effec482ad24a4a98872da7275
Vagiza Svidchenkova <w.svidchenkova@gmail.com>
2025-07-17 00:30:14 +0400

    Подключает MainEmpty в main.tsx

--- src/view/pages/main/main.tsx
+++ src/view/pages/main/main.tsx
@@ -4,12 +4,14 @@ import PlaceList from '../../components/place-list/place-list.tsx';
 import Map from '../../components/map/map.tsx';
 import { useAppSelector } from '../../../hooks/index.ts';
 import { getFilteredCityOffers } from '../../../utils.ts';
-import { selectCity, selectOffers } from '../../../store/selectors/offers.ts';
+import { selectCity, selectErrorStatus, selectOffers } from '../../../store/selectors/offers.ts';
 import { OfferType } from '../../../types/offer.ts';
+import MainEmpty from '../../components/main-empty/main-empty.tsx';
 
 function Main(): JSX.Element {
   const city = useAppSelector(selectCity);
   const offers = useAppSelector(selectOffers);
+  const hasError = useAppSelector(selectErrorStatus);
   const filteredOffers = getFilteredCityOffers(offers, city);
 
   const [activeOffer, setActiveOffer] = useState<OfferType | undefined>(undefined);
@@ -18,23 +20,20 @@ function Main(): JSX.Element {
   };
 
   return (
-    <main className="page__main page__main--index">
+    <main className={`page__main page__main--index ${hasError && 'page__main--index-empty'}`}>
       <h1 className="visually-hidden">Cities</h1>
       <div className="tabs">
         <LocationsList />
       </div>
       <div className="cities">
-        <div className="cities__places-container container">
-          <PlaceList
-            offers={filteredOffers}
-            onOfferHover={handleOfferHover}
-          />
+        <div
+          className={`cities__places-container ${hasError && 'cities__places-container--empty'} container`}
+        >
+          {hasError
+            ? <MainEmpty city={city} />
+            : <PlaceList offers={filteredOffers} onOfferHover={handleOfferHover} />}
           <div className="cities__right-section">
-            <Map
-              offers={filteredOffers}
-              activeOffer = {activeOffer}
-              mapName='CITIES'
-            />
+            {!hasError && <Map offers={filteredOffers} activeOffer = {activeOffer} mapName='CITIES' />}
           </div>
         </div>
       </div>
